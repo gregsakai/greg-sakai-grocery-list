7d724e2abb8755020349c2cc04bf4e8d225326d9
gregsakai <gregsakai@gmail.com>
2017-10-13 16:42:42 -0700

    added control and lists

---
+++ src/comp/GControl.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class GControl extends Component {
+
+
+
+  constructor(props){
+    super(props);
+
+
+  }
+
+  render() {
+
+
+
+
+
+    return (
+      <div className="Control">
+
+      </div>
+    );
+  }
+
+
+}
+
+export default GControl;
---
+++ src/comp/GLists.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class GLists extends Component {
+
+
+
+  constructor(props){
+    super(props);
+
+
+  }
+
+  render() {
+
+
+
+
+
+    return (
+      <div className="Lists">
+
+      </div>
+    );
+  }
+
+
+}
+
+export default GLists;
